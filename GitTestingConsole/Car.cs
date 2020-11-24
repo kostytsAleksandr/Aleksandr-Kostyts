5f7a1919ff6e589561e971d7d873950e8fd2afe9
kostytsAleksandr <kostyts.aleksandr@gmail.com>
2020-11-24 21:18:36 +0200

    ThirdCommit

---
+++ GitTestingConsole/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestingConsole
+{
+    class Car
+    {
+
+        public int Age { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public void Show()
+        {
+            Console.WriteLine();
+        }
+    }
+}
--- GitTestingConsole/Program.cs
+++ GitTestingConsole/Program.cs
@@ -11,6 +11,7 @@ namespace GitTestingConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World");
+
             Console.ReadKey();
         }
     }
