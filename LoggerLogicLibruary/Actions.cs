0e63d19e2c0bf52f608efbf592382e7a825bffcf
kostytsAleksandr <kostyts.aleksandr@gmail.com>
2020-11-26 21:12:25 +0200

    Logger test first commit

---
+++ LoggerLogicLibruary/Actions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerLogicLibruary
+{
+    public class Actions
+    {
+        public Actions(string pathTofile)
+        {
+            _logger = Logger.CreateLogger(pathTofile);
+        }
+
+        private Logger _logger;
+
+        public void Start()
+        {
+            _logger.IsInfo = true;
+            Console.WriteLine(_logger.ShowLoggerInfo("Start"));
+        }
+
+        public void WarningSituation()
+        {
+            _logger.IsWarning = true;
+            Console.WriteLine(_logger.ShowLoggerInfo("Skipped logic in method"));
+        }
+
+        public void ErrorSituation()
+        {
+            _logger.IsError = true;
+            _logger.ShowLoggerInfo("I broke a toilet");
+            throw new Exception("I broke a toilet");
+        }
+    }
+}
---
+++ LoggerLogicLibruary/Logger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerLogicLibruary
+{
+    public class Logger
+    {
+        public static Logger CreateLogger(string pathToWriteInfo)
+        {
+            return new Logger(pathToWriteInfo);
+        }
+        private Logger(string pathToWriteInfo)
+        {
+            _pathToFile = pathToWriteInfo;
+            IsError = false;
+            IsInfo = false;
+            IsWarning = false;
+        }
+
+        private readonly string _pathToFile;
+        public bool IsError { get; set; }
+        public bool IsWarning { get; set; }
+        public bool IsInfo { get; set; }
+
+        public string ShowLoggerInfo(string message)
+        {
+            string loggerText = string.Empty;
+            if (IsError)
+            {
+                IsWarning = false;
+                IsInfo = false;
+                loggerText= $"{DateTime.Now}: Error: {message}";
+            }
+            if (IsWarning)
+            {
+                IsInfo = false;
+                IsError = false;
+                loggerText = $"{DateTime.Now}: Warning: {message}";
+            }
+            if (IsInfo)
+            {
+                IsWarning = false;
+                IsError = false;
+                loggerText = $"{DateTime.Now}: Info: {message}";
+            }
+            File.AppendAllText(_pathToFile, loggerText + Environment.NewLine);
+            return loggerText;
+        }
+    }
+}
