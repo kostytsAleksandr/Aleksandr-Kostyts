77068d503ee88f4ef5d3ca17833c97d23393a2d2
kostytsAleksandr <kostyts.aleksandr@gmail.com>
2020-12-11 11:23:43 +0200

    EmptyProject

---
+++ SafariParkConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SafariParkConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
